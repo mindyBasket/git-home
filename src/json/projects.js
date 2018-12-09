6528110d450d4b40f5040f581352d78ef254696d
blacksandbox <sandcubed@gmail.com>
2018-12-08 23:58:19 -0500

    Convert each card into its own component to track its own toggle state

--- src/json/projects.js
+++ src/json/projects.js
@@ -1,4 +1,4 @@
-var projects = {
+var projectData = {
 
   	projects: [
   		{
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,10 +3,54 @@ console.log("Script loaded");
 //var local_data = data;
 //console.log(JSON.stringify(local_data));
 
+
+
+// Note: components must come first before "new Vue"
+
+
+Vue.component('project-card', {
+
+    props:{
+    	proj: Object
+  	},
+  	data() {
+	    return {
+	      	isActive: false
+	    }
+	},
+  	methods: {
+	  	openCard: function(){
+	  		this.isActive = !this.isActive;
+	  	}
+	},
+  	template: `
+		<div class="card project" 
+			 v-on:click="openCard"
+			 v-bind:class="{ 'active': isActive }">
+
+			<div class="thumbnail">
+				<img src=""/>
+			</div>
+			<div class="title">{{proj.title}}</div>
+
+			<div class="short_summary">
+				<div class="summary">{{proj.shortSummary}}</div>
+				<div class="tags flex_row wrap">
+					<span v-for="tag in proj.tags">{{tag}}</span>
+				</div>
+			</div>
+			<div class="long_summary">{{proj.longSummary}}</div>
+		</div>
+	`
+
+		
+});
+
+
 var vm = new Vue({
   el: '#projects1',
   data: {
-  	projects: projects,
+  	projectData: projectData,
   	isActive: false
   },
 
@@ -16,4 +60,4 @@ var vm = new Vue({
   	}
   }
 
-})
+});
