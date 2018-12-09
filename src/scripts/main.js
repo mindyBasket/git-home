0d22fc91998ff8a2de5a02a735ebad1995f90cc2
blacksandbox <sandcubed@gmail.com>
2018-12-09 17:52:02 -0500

    Make card's height transition into calculatd "auto" after expanding width

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -19,10 +19,45 @@ Vue.component('project-card', {
 	      	isActive: false,
           isOpened: false,
 
+          elStyle: null,
+          defaultHeight: null,
+
           to_showSummary: null,
 	    }
-	 },
+	  }
+    ,
+    mounted(){
+      // add style info
+      // Note: getComputedStyle properties come with units like "px"
+      this.elStyle = getComputedStyle(this.$el);
+      this.defaultHeight = this.elStyle.height;
+    }
+    ,
   	methods: {
+      // transition hooks
+      longSum_enter: function(el, done){
+
+        // Quickly turn card to height:auto to get the full height
+        this.$el.style.height = 'auto';
+        const expandHeight = getComputedStyle(this.$el).height; 
+
+        this.$el.style.height = this.defaultHeight;
+        console.log(`Defaulted to ${this.$el.style.height}`);
+        // Trigger the animation: use `setTimeout` to make sure the element finished
+        // painting after setting height back to defaultHeight
+        setTimeout(() => {
+          this.$el.style.height = expandHeight;
+          console.log(`Expand to ${expandHeight}`);
+        });
+
+        done();
+      }
+      ,
+      longSum_leave: function(el, done){
+        this.$el.style.height = this.defaultHeight;
+        done();
+      }
+      ,
 	  	openCard: function(){
 	  		if (this.isActive){
 	  			vm.removeCurrFiller();
@@ -53,9 +88,13 @@ Vue.component('project-card', {
 				<div class="summary">{{proj.shortSummary}}</div>
 			</div>
 
-      <transition name="show-long-summary">
-        <div v-if="isOpened"
-             class="long_summary">{{proj.longSummary}}</div>
+      <transition v-on:enter="longSum_enter"
+                  v-on:leave="longSum_leave">
+        <div class="long_summary"
+             v-if="isOpened"
+             >
+             {{proj.longSummary}}
+        </div>
       </transition>
 			
 		</div>
@@ -72,8 +111,11 @@ var vm = new Vue({
   	isActive: false
   },
 
+
+
   methods: {
   	afterEnter: function(el){
+      /* shrink filler card after being inserted */
   		el.className += " shrink";
   	},
   	duplicateProjObj: function(ind){
@@ -83,14 +125,14 @@ var vm = new Vue({
   		return copyProj;
   	},
   	addFiller: function (ind) {
-  		console.log("Add filler");
-      	this.projectData.projects.splice(ind+1, 0, this.duplicateProjObj(0) );
+  		//console.log("Add filler");
+      this.projectData.projects.splice(ind+1, 0, this.duplicateProjObj(0) );
     },
     removeCurrFiller: function () {
     	const targetInd = Number(document.querySelector(".project.filler").getAttribute("index"));
     	console.log(targetInd);
     	if (targetInd != null || targetInd === undefined){
-    		console.log(`Remove filler at ${targetInd}`);
+    		//console.log(`Remove filler at ${targetInd}`);
     		this.projectData.projects.splice(targetInd, 1);
     	}
       	
