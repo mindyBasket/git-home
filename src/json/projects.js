1f263bf07c3fad5914b5201aa1a0b4d52aef69f8
blacksandbox <sandcubed@gmail.com>
2018-12-09 00:25:32 -0500

    Update content

--- src/json/projects.js
+++ src/json/projects.js
@@ -3,10 +3,21 @@ var projectData = {
   	projects: [
   		{
   			img: "",
-  			title: "VEX Education Portal",
-  			shortSummary: "STEM course content delivery platform that uses VEX. Involved in web development, course development and deployment.",
+  			title: "[Secret ongoing project]",
+  			shortSummary: "Building a mobile app that uses visual programming language to introduce children to robotics hardware and software",
   			tags: [
-  				"Javascript",
+  				"ReactJS",
+				"Unity",
+				"Blockly"
+  			],
+  			longSummary: "Long content Because long contents are unique to each project, I am not too sure how to store the information :(. Involved in second iteration of Badge system. Including system that reward different tiers of badge and ways to display and share. Logic parser for interpreting trigger rule."
+  		}
+  		,
+  		{
+  			img: "",
+  			title: "VEX Education Portal",
+  			shortSummary: "Built a STEM course content delivery platform as well as the course content development and deployment",
+  			tags: [
 				"PHP",
 				"Node.js",
 				"MySQL",
@@ -19,7 +30,7 @@ var projectData = {
   		{
   			img: "",
   			title: "Flipbook",
-  			shortSummary: "Editor that generates sharable 'flipbooks', an alternative story telling format that is somewhere between an animated movie and a storyboard.",
+  			shortSummary: "Build an app that generates sharable 'flipbooks', an alternative story telling format that is somewhere between an animated movie and a storyboard",
   			tags: [
   				"ReactJS",
   				"Django",
@@ -37,19 +48,22 @@ var projectData = {
   		{
   			img: "/images/project_thumbs/cs2n_1.png",
   			title: "CS2N",
-  			shortSummary: "Involved in second iteration of Badge system. Including system that reward different tiers of badge and ways to display and share. Logic parser for interpreting trigger rule.",
+  			shortSummary: "Worked on the second iteration of the badge reward system, both back end and front end",
   			tags: [
   				"Ruby on Rails",
   				"Javascript",
   				"mySQL",
   			],
-  			longSummary: `Morbi elementum facilisis hendrerit. Nam ullamcorper tristique nisi lobortis pulvinar. Proin eu sapien quis elit blandit tincidunt id eu arcu. Vestibulum vestibulum magna cursus ex luctus, ac efficitur felis porttitor. Mauris consectetur aliquet tortor ac sollicitudin. Proin nec tempor lorem. Nam a varius diam. Pellentesque feugiat dictum lacinia. Curabitur quis viverra sapien, sit amet accumsan lorem. Praesent orci ipsum, aliquam in lectus a, laoreet mattis mi. Morbi mollis dolor lacus, eu pulvinar dolor posuere non. `
+  			longSummary: `
+  				which rewards different tiers of badge and ways to display and share. Logic parser for interpreting trigger rule.
+
+  				Morbi elementum facilisis hendrerit. Nam ullamcorper tristique nisi lobortis pulvinar. Proin eu sapien quis elit blandit tincidunt id eu arcu. Vestibulum vestibulum magna cursus ex luctus, ac efficitur felis porttitor. Mauris consectetur aliquet tortor ac sollicitudin. Proin nec tempor lorem. Nam a varius diam. Pellentesque feugiat dictum lacinia. Curabitur quis viverra sapien, sit amet accumsan lorem. Praesent orci ipsum, aliquam in lectus a, laoreet mattis mi. Morbi mollis dolor lacus, eu pulvinar dolor posuere non. `
   		}
   		,
   		{
   			img: "/images/project_thumbs/cs2n_1.png",
   			title: "RoboAcademy Curriculum Products",
-  			shortSummary: "A family of lightweight, standalone curriculum sites. Made everything on it, from web dev, interactive widgets, course video and graphics",
+  			shortSummary: "Build a family of lightweight, standalone curriculum sites. Did everything for it, from web dev, interactive widgets, course video and graphics",
   			tags: [
   				"Javascript",
   				"bootstrap",
@@ -59,7 +73,7 @@ var projectData = {
   		{
   			img: "",
   			title: "Sandbloqs",
-  			shortSummary: "Extended Google's Blockly to have 'Object' type. Intended to teach object oriented programming language.",
+  			shortSummary: "Built an app that utilizes Google's Blockly. Intended to teach object oriented programming language.",
   			tags: [
   				"Javascript",
   				"Phaser",
@@ -73,11 +87,11 @@ var projectData = {
   		{
   			img: "",
   			title: "Robomatter Curriculum Products",
-  			shortSummary: "Madcap is a documentation program that we extended its usage to behave like Moodle. Javascript was used to fill the gap between desired UI/UX functionality to what Madcap can offer.",
+  			shortSummary: "Assisted building course delivery platform by writing course content and making plugins and widgets that original software could not fill in",
   			tags: [
   				"Javascript",
   			],
-  			longSummary: `Morbi elementum facilisis hendrerit. Nam ullamcorper tristique nisi lobortis pulvinar. Proin eu sapien quis elit blandit tincidunt id eu arcu. Vestibulum vestibulum magna cursus ex luctus, ac efficitur felis porttitor. Mauris consectetur aliquet tortor ac sollicitudin. Proin nec tempor lorem. Nam a varius diam. Pellentesque feugiat dictum lacinia. Curabitur quis viverra sapien, sit amet accumsan lorem. Praesent orci ipsum, aliquam in lectus a, laoreet mattis mi. Morbi mollis dolor lacus, eu pulvinar dolor posuere non. `
+  			longSummary: `Madcap is a documentation program that we extended its usage to behave like Moodle. Javascript was used to fill the gap between desired UI/UX functionality to what Madcap can offer. `
   		}
 
   	]
