909b22d1504984d461af7956a992585a536a6a2f
blacksandbox <sandcubed@gmail.com>
2018-12-08 22:04:47 -0500

    Use vue.js to list the projects from projects json

--- src/json/projects.js
+++ src/json/projects.js
@@ -1,5 +1,84 @@
-var data = {
+var projects = {
 
-  hello: "world"
+  	projects: [
+  		{
+  			img: "",
+  			title: "VEX Education Portal",
+  			shortSummary: "STEM course content delivery platform that uses VEX. Involved in web development, course development and deployment.",
+  			tags: [
+  				"Javascript",
+				"PHP",
+				"Node.js",
+				"MySQL",
+				"Python",
+				"DevOps"
+  			],
+  			longSummary: "Long content Because long contents are unique to each project, I am not too sure how to store the information :(. Involved in second iteration of Badge system. Including system that reward different tiers of badge and ways to display and share. Logic parser for interpreting trigger rule."
+  		}
+  		,
+  		{
+  			img: "",
+  			title: "Flipbook",
+  			shortSummary: "Editor that generates sharable 'flipbooks', an alternative story telling format that is somewhere between an animated movie and a storyboard.",
+  			tags: [
+  				"ReactJS",
+  				"Django",
+  				"RESTful framework",
+  				"Node.js",
+  				"Heroku"
+  			],
+  			longSummary: `A project started to teach myself Django and React. Started out as a Trello imitation but slowly found its purpose as an editor that creates an experimental form of media called "flipbook" (name subject to change)
 
+									Flipbooks are intended to create something that appear scenamatic, but is low cost since it is not an actual movie. 
+
+									The app is still at its infancy so currently lacks any user data. `
+  		}
+  		,
+  		{
+  			img: "/images/project_thumbs/cs2n_1.png",
+  			title: "CS2N",
+  			shortSummary: "Involved in second iteration of Badge system. Including system that reward different tiers of badge and ways to display and share. Logic parser for interpreting trigger rule.",
+  			tags: [
+  				"Ruby on Rails",
+  				"Javascript",
+  				"mySQL",
+  			],
+  			longSummary: `Morbi elementum facilisis hendrerit. Nam ullamcorper tristique nisi lobortis pulvinar. Proin eu sapien quis elit blandit tincidunt id eu arcu. Vestibulum vestibulum magna cursus ex luctus, ac efficitur felis porttitor. Mauris consectetur aliquet tortor ac sollicitudin. Proin nec tempor lorem. Nam a varius diam. Pellentesque feugiat dictum lacinia. Curabitur quis viverra sapien, sit amet accumsan lorem. Praesent orci ipsum, aliquam in lectus a, laoreet mattis mi. Morbi mollis dolor lacus, eu pulvinar dolor posuere non. `
+  		}
+  		,
+  		{
+  			img: "/images/project_thumbs/cs2n_1.png",
+  			title: "RoboAcademy Curriculum Products",
+  			shortSummary: "A family of lightweight, standalone curriculum sites. Made everything on it, from web dev, interactive widgets, course video and graphics",
+  			tags: [
+  				"Javascript",
+  				"bootstrap",
+  			],
+  			longSummary: `Morbi elementum facilisis hendrerit. Nam ullamcorper tristique nisi lobortis pulvinar. Proin eu sapien quis elit blandit tincidunt id eu arcu. Vestibulum vestibulum magna cursus ex luctus, ac efficitur felis porttitor. Mauris consectetur aliquet tortor ac sollicitudin. Proin nec tempor lorem. Nam a varius diam. Pellentesque feugiat dictum lacinia. Curabitur quis viverra sapien, sit amet accumsan lorem. Praesent orci ipsum, aliquam in lectus a, laoreet mattis mi. Morbi mollis dolor lacus, eu pulvinar dolor posuere non. `
+  		},
+  		{
+  			img: "",
+  			title: "Sandbloqs",
+  			shortSummary: "Extended Google's Blockly to have 'Object' type. Intended to teach object oriented programming language.",
+  			tags: [
+  				"Javascript",
+  				"Phaser",
+  				"NW.js",
+  				"Blockly",
+
+  			],
+  			longSummary: `Morbi elementum facilisis hendrerit. Nam ullamcorper tristique nisi lobortis pulvinar. Proin eu sapien quis elit blandit tincidunt id eu arcu. Vestibulum vestibulum magna cursus ex luctus, ac efficitur felis porttitor. Mauris consectetur aliquet tortor ac sollicitudin. Proin nec tempor lorem. Nam a varius diam. Pellentesque feugiat dictum lacinia. Curabitur quis viverra sapien, sit amet accumsan lorem. Praesent orci ipsum, aliquam in lectus a, laoreet mattis mi. Morbi mollis dolor lacus, eu pulvinar dolor posuere non. `
+  		},
+
+  		{
+  			img: "",
+  			title: "Robomatter Curriculum Products",
+  			shortSummary: "Madcap is a documentation program that we extended its usage to behave like Moodle. Javascript was used to fill the gap between desired UI/UX functionality to what Madcap can offer.",
+  			tags: [
+  				"Javascript",
+  			],
+  			longSummary: `Morbi elementum facilisis hendrerit. Nam ullamcorper tristique nisi lobortis pulvinar. Proin eu sapien quis elit blandit tincidunt id eu arcu. Vestibulum vestibulum magna cursus ex luctus, ac efficitur felis porttitor. Mauris consectetur aliquet tortor ac sollicitudin. Proin nec tempor lorem. Nam a varius diam. Pellentesque feugiat dictum lacinia. Curabitur quis viverra sapien, sit amet accumsan lorem. Praesent orci ipsum, aliquam in lectus a, laoreet mattis mi. Morbi mollis dolor lacus, eu pulvinar dolor posuere non. `
+  		}
+
+  	]
 }
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,10 @@
 console.log("Script loaded");
 
-var local_data = data;
-console.log(JSON.stringify(local_data));
+//var local_data = data;
+//console.log(JSON.stringify(local_data));
+
+var vm = new Vue({
+  el: '#projects1',
+  data: projects
+
+})
