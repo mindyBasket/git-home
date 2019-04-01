39eb4a8841f31e30391cd5adaae3ce0f7fb3296d
mindyBox <sandcubed@gmail.com>
2019-04-01 01:35:39 -0400

    Redfine content in activity section and make minor style adjustments

--- src/data/data_activities.js
+++ src/data/data_activities.js
@@ -4,15 +4,16 @@ var activityData = {
     {
       title: "Global Game Jam",
       year: ["2016", "Present"],
-      description: `Participated in Global Game Jams in Pittsburgh with a duel role of an artist and a programmer.
-       The team I joined achieved "Visual Excellence Award" in 2018, and "Non-traditional Award" in 2019.`
+      description: `Participated in Global Game Jams in Pittsburgh with a <strong>duel role</strong>
+       of a graphic artist and a programmer. The team I joined achieved 
+       <strong>"Visual Excellence Award" in 2018</strong>, and <strong>"Non-traditional Award" in 2019</strong>.`
     },
     {
       title: "Machine Learning Projects",
       year: ["2017", "2018"],
       description: `Finished an online course at Udacity that required completion of 6 projects
-      on Robotics Software, and Deep Neural Networks. Acquired and utilized knowledge on machine learning 
-      concepts (CNN, Rtab-mapping, and RL), and libraries and tools (ROS, Rviz, Gazebo). 
+      on Robotics Software, and Deep Neural Networks. It tested my knowledge on machine learning 
+      concepts (CNN, Rtab-mapping, and RL), and tools used for simulating and testing it (ROS, Rviz, Gazebo). 
       A notable project is hand-sign recognition model that I trained to 
       recognize 6 different hand signs. Used PyTorch and Jupyter to build. 
       My cohorts were the first to take this coures.`
@@ -20,7 +21,8 @@ var activityData = {
     {
       title: "Programming Judge",
       year: ["2014", "Present"],
-      description: `Programming judge: at FLL and VEX Robotics Competition`
+      description: `I was one of the <strong>programming judge</strong> in various local and national
+      robotics competitions such as FLL and VEX Robotics Competitions.`
     },
   ]
 }
